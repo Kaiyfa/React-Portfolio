f64093cac1dcbe74ac9036a4ec405acfb77ce14d
Adama Sall <115763652+Kaiyfa@users.noreply.github.com>
2023-03-03 05:54:46 +0000

    Update home.jsx

--- portfolio-app/src/pages/home.jsx
+++ portfolio-app/src/pages/home.jsx
@@ -18,7 +18,7 @@ function Home() {
 
                         <p className="lead">Visit my portfolio to see how far I've come on my journey.</p>
                         <div className="cta">
-                            <a href='/portfolio'className="btn btn-primary" >Portfolio</a>
+                            <a href='/portfolio'className="btn btn-primary" target='portfolio' >Portfolio</a>
                             <br />
                             <a href={CV} className="btn btn-primary" target="blank" > My
                                 Resume</a>
